d049c44b25add9c89005510db7cd3f702c94eab8
Andrey <46047628+deystroyter@users.noreply.github.com>
2020-05-31 20:29:00 +0300

    First version

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Runtime.InteropServices;
+
+namespace FedorovAD___Pz_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FilePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
+            using (var FStream = new FileStream(Path.Combine(FilePath, "input.txt"), FileMode.Open))
+            using (var SReader = new StreamReader(FStream))
+            {
+                int n = int.Parse(SReader.ReadLine());
+                Software[] SoftDataBase = new Software[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    string recordingLine = SReader.ReadLine();
+                    Software record = GetRecordType(recordingLine);
+                    SoftDataBase[i] = record;
+                }
+                FindFullSoft(SoftDataBase);
+                FindAvailableSoft(SoftDataBase);
+                Console.ReadLine();
+            }
+        }
+        /// <summary>
+        /// Вывод всех программ из массива софта
+        /// </summary>
+        /// <param name="Db">Массив записей</param>
+        public static void FindFullSoft(Software[] Db)
+        {
+            Console.WriteLine("<<<< Полный список софта >>>>");
+            Console.WriteLine("-------------------");
+            foreach (Software elem in Db)
+            {
+                    elem.GetInfo();
+                    Console.WriteLine("-------------------");
+            }
+        }
+        /// <summary>
+        /// Вывод доступных программ из массива софта
+        /// </summary>
+        /// <param name="Db">Массив записей</param>
+        public static void FindAvailableSoft(Software[] Db)
+        {
+            Console.WriteLine("<<<< Список доступного софта >>>>");
+            Console.WriteLine("-------------------");
+            foreach (Software elem in Db)
+            {
+                if (elem.GetAvailability())
+                {
+                    elem.GetInfo();
+                    Console.WriteLine("-------------------");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Узнает тип программного обеспечения и создаёт новый элемент этого класса
+        /// </summary>
+        /// <param name="recordingLine">Строка из файла input.txt</param>
+        public static Software GetRecordType(string recordingLine)
+        {
+            var vals = recordingLine.Split('|');
+            switch (vals[0])
+            {
+                case "FreeSoft":
+                    return new FreeSoft(vals[1], vals[2]);
+                case "Shareware":
+                    return new Shareware(vals[1], vals[2], DateTime.Parse(vals[3]), uint.Parse(vals[4]));
+                case "CommercialSoft":
+                    return new CommercialSoft(vals[1], vals[2], float.Parse(vals[3]), DateTime.Parse(vals[4]), uint.Parse(vals[5]));
+                default:
+                    throw new Exception("привет! я ошибочка");
+            }
+        }
+    }
+}
---
+++ Software.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FedorovAD___Pz_2
+{
+    abstract class Software
+    {
+        /// <summary>
+        /// Название программного обеспечения
+        /// </summary>
+        protected string Name;
+        /// <summary>
+        /// Издатель программного обеспечения
+        /// </summary>
+        protected string Creator;
+        /// <summary>
+        /// Доступность программного обеспечения
+        /// </summary>
+        protected bool Availability;
+        /// <summary>
+        /// Получить информацию об ПО
+        /// </summary>
+        public abstract void GetInfo();
+        /// <summary>
+        /// Получить информацию о доступности об ПО
+        /// </summary>
+        public abstract bool GetAvailability();
+    }
+    /// <summary>
+    /// Класс для бесплатного ПО
+    /// </summary>
+    class FreeSoft : Software
+    {
+        /// <summary>
+        /// Конструктор класса FreeSoft
+        /// </summary>
+        public FreeSoft(string SoftName,string SoftCreator)
+        {
+            Name = SoftName;
+            Creator = SoftCreator;
+            Availability = true;
+
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Название ПО: {Name}");
+            Console.WriteLine($"Издатель: {Creator}");
+        }
+        public override bool GetAvailability()
+        {
+            return Availability;
+        }
+    }
+    /// <summary>
+    /// Класс для условно-бесплатного ПО
+    /// </summary>
+    class Shareware : Software
+    {
+        /// <summary>
+        /// Дата установки ПО
+        /// </summary>
+        DateTime InstallDate;
+        /// <summary>
+        /// Дней бесплатной лицензии
+        /// </summary>
+        uint FreeLicenseDays;
+        /// <summary>
+        /// Конструктор класса Shareware
+        /// </summary>
+        public Shareware(string SoftName, string SoftCreator, DateTime SoftInstallDate, uint SoftDays)
+        {
+            Name = SoftName;
+            Creator = SoftCreator;
+            InstallDate = SoftInstallDate;
+            FreeLicenseDays = SoftDays;
+
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Название ПО: {Name}");
+            Console.WriteLine($"Издатель: {Creator}");
+            Console.WriteLine($"Дата установки: {InstallDate}");
+            Console.WriteLine($"Дней бесплатной лицензии: {FreeLicenseDays}");
+        }
+        public override bool GetAvailability()
+        {
+            if (InstallDate.AddDays(FreeLicenseDays) < DateTime.Now)
+            { 
+                Availability = false;
+                return Availability;
+            }
+            else 
+            { 
+                Availability = true;
+                return Availability;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Класс для платного ПО
+    /// </summary>
+    class CommercialSoft : Software
+    {
+        /// <summary>
+        /// Дата установки ПО
+        /// </summary>
+        DateTime InstallDate;
+        /// <summary>
+        /// Дней лицензии
+        /// </summary>
+        uint LicenseDays;
+        /// <summary>
+        /// Цена
+        /// </summary>
+        float Price;
+
+        public CommercialSoft(string SoftName, string SoftCreator, float SoftPrice, DateTime SoftInstallDate, uint SoftDays)
+        {
+            Name = SoftName;
+            Creator = SoftCreator;
+            Price = SoftPrice;
+            InstallDate = SoftInstallDate;
+            LicenseDays = SoftDays;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Название ПО: {Name}");
+            Console.WriteLine($"Издатель: {Creator}");
+            Console.WriteLine($"Цена: {Price}");
+            Console.WriteLine($"Дата установки: {InstallDate}");
+            Console.WriteLine($"Дней лицензии: {LicenseDays}");
+        }
+        public override bool GetAvailability()
+        {
+            if (InstallDate.AddDays(LicenseDays) < DateTime.Now)
+            {
+                Availability = false;
+                return Availability;
+            }
+            else
+            {
+                Availability = true;
+                return Availability;
+            }
+        }
+    }
+}
